461027903e9e1aa54b6cda5299eea3aab4ca1720
hajlaoui-nader <hajlaoui.nader@gmail.com>
2019-05-31 09:56:52 +0200

    init

--- src/main/scala/com/stuart/first/Main.scala
+++ src/main/scala/com/stuart/first/Main.scala
@@ -2,4 +2,23 @@ package com.stuart.first
 
 // this is a main class, it may be the project entry point
 // what we need to memorize here is the second part of the object declaration (extends App)
-object Main extends App {}
+object Main extends App {
+
+  // variables assignment
+  // what is val ?
+  val message = ???
+  // what is var ?
+  var message2 = ???
+
+  // factorial(x) = x * x-1 * x-2 * ... * (fact(0) == 1)
+  def factorial(x: BigInt): BigInt = ???
+
+  //Question:
+  // Write a program which repeatedly prompts the user for an integer.
+  // If the integer is even, print the integer.
+  // If the integer is odd, don’t print anything.
+  // Exit the program if the user enters the integer 33.
+  def myMethod() = ???
+
+}
+// Q: Design a class which represents a rectangle, and provides calculateArea method.
