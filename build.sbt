f56198ee28f291f4c20d3344fbe10b6110d23a39
hajlaoui-nader <hajlaoui.nader@gmail.com>
2019-05-31 09:35:50 +0200

    init

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "MobP"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
---
+++ src/main/scala/com/stuart/first/Main.scala
@@ -0,0 +1,5 @@
+package com.stuart.first
+
+// this is a main class, it may be the project entry point
+// what we need to memorize here is the second part of the object declaration (extends App)
+object Main extends App {}
